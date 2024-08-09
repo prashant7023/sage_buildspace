bb8c53dbb4562c65d950d4d7dd72e88a6b912acf
Prashant Singh <137259400+prashant7023@users.noreply.github.com>
2024-08-09 11:52:31 +0530

    Update main.tsx

--- next/components/main.tsx
+++ next/components/main.tsx
@@ -1,44 +1,164 @@
-import React from 'react'
+import React from 'react';
 
-const sidebar = () => {
+const Carousel: React.FC = () => {
   return (
-    <div>
-    <div  className="fixed inset-y-0 hidden w-[256px] border-r border-dashed bg-card py-6 pl-6 transition-all md:block">
-      <div  className="relative flex h-full flex-col gap-2">
-        <a  className="pl-2" href="/">
-          <div  className="flex w-full items-center">
-            <svg width="28" height="28" viewBox="0 0 142 120" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M56.7217 9.77302L62.3521 3.93267C66.3898 -0.739584 74.159 -1.71578 79.108 3.56807L138.745 67.2401C143.66 72.4872 142.378 80.9182 136.123 84.4863L76.4861 118.507C72.3766 120.851 67.7095 120.281 64.3443 117.867L5.62655 84.3704C-0.628203 80.8023 -1.90994 72.3713 3.00467 67.1242C5.26775 64.7081 8.90886 64.1658 11.7845 65.8167L64.3443 95.8885V73.2996L30.0497 53.8039C25.2072 51.0184 24.2219 44.4809 28.0298 40.4061L28.0368 40.3986L28.0424 40.3926C30.8094 37.444 35.2569 36.7833 38.7699 38.8002L64.3443 53.2077V31.4034L56.2082 26.3126C51.2242 23.4513 46.7253 20.4457 50.6477 16.258L56.7217 9.77302Z" fill="currentColor"></path>
-            </svg>
+    <main className=" flex w-full flex-1 flex-col md:pl-[272px] lg:pr-0">
+      <div className="pt-8 min-h-[calc(100dvh-57px)] md:min-h-dvh flex flex-col gap-16">
+        <div className="my-auto mx-auto flex flex-col gap-16">
+          <h1 className="text-5xl font-extrabold tracking-tighter sm:text-7xl text-center">
+            find and<br />be found.
+          </h1>
+          <div className="flex flex-col gap-4">
+            {/* Carousel for People */}
+            <div className="relative h-64 max-w-sm md:max-w-md lg:max-w-2xl xl:max-w-4xl" role="region" aria-roledescription="carousel">
+              <div className="overflow-hidden">
+                <div className="flex -ml-4" style={{ transform: 'translate3d(-1349.3px, 0px, 0px)' }}>
+                  {/* Slide 1 */}
+                  <div role="group" aria-roledescription="slide" className="min-w-0 shrink-0 grow-0 basis-full pl-4 max-w-48" style={{ transform: 'translate3d(0px, 0px, 0px)' }}>
+                    <div className="border text-card-foreground shadow-md group h-64 bg-transparent cursor-pointer opacity-80 hover:opacity-100">
+                      <div className="p-3 flex flex-col gap-0">
+                        <span className="group relative flex shrink-0 items-center justify-center overflow-hidden border bg-secondary size-14 w-full min-h-[96px] mb-2" style={{ position: 'absolute', height: '100%', width: '100%', inset: '0px', color: 'transparent' }}>
+                          <img
+                            alt="Mattia"
+                            loading="lazy"
+                            decoding="async"
+                            src="https://sapi.buildspace.so/storage/v1/render/image/public/assets/17d64dd1-0b8e-44d6-b68c-cb5ceb6d706b/avatar?width=3840&resize=contain&quality=75"
+                            className="aspect-square size-full object-cover grayscale group-hover:grayscale-0"
+                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                          />
+                        </span>
+                        <h3 className="tracking-tight line-clamp-1 text-left font-medium text-lg lg:text-lg">Mattia</h3>
+                      </div>
+                      <div className="p-3 pt-0">
+                        <h4 className="font-semibold tracking-tight line-clamp-3 truncate whitespace-break-spaces text-muted-foreground text-sm pb-0 text-left">
+                          building a visual landing page builder for developers
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Slide 2 */}
+                  <div role="group" aria-roledescription="slide" className="min-w-0 shrink-0 grow-0 basis-full pl-4 max-w-48" style={{ transform: 'translate3d(0px, 0px, 0px)' }}>
+                    <div className="border text-card-foreground shadow-md group h-64 bg-transparent cursor-pointer opacity-80 hover:opacity-100">
+                      <div className="p-3 flex flex-col gap-0">
+                        <span className="group relative flex shrink-0 items-center justify-center overflow-hidden border bg-secondary size-14 w-full min-h-[96px] mb-2" style={{ position: 'absolute', height: '100%', width: '100%', inset: '0px', color: 'transparent' }}>
+                          <img
+                            alt="Kelli"
+                            loading="lazy"
+                            decoding="async"
+                            src="https://sapi.buildspace.so/storage/v1/render/image/public/assets/17d64dd1-0b8e-44d6-b68c-cb5ceb6d706b/avatar?width=3840&resize=contain&quality=75"
+                            className="aspect-square size-full object-cover grayscale group-hover:grayscale-0"
+                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                          />
+                        </span>
+                        <h3 className="tracking-tight line-clamp-1 text-left font-medium text-lg lg:text-lg">Kelli</h3>
+                      </div>
+                      <div className="p-3 pt-0">
+                        <h4 className="font-semibold tracking-tight line-clamp-3 truncate whitespace-break-spaces text-muted-foreground text-sm pb-0 text-left">
+                          building a visual landing page builder for developers
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Slide 3 */}
+                  <div role="group" aria-roledescription="slide" className="min-w-0 shrink-0 grow-0 basis-full pl-4 max-w-48" style={{ transform: 'translate3d(0px, 0px, 0px)' }}>
+                    <div className="border text-card-foreground shadow-md group h-64 bg-transparent cursor-pointer opacity-80 hover:opacity-100">
+                      <div className="p-3 flex flex-col gap-0">
+                        <span className="group relative flex shrink-0 items-center justify-center overflow-hidden border bg-secondary size-14 w-full min-h-[96px] mb-2" style={{ position: 'absolute', height: '100%', width: '100%', inset: '0px', color: 'transparent' }}>
+                          <img
+                            alt="Alec"
+                            loading="lazy"
+                            decoding="async"
+                            src="https://sapi.buildspace.so/storage/v1/render/image/public/assets/477b3b94-21b0-4202-a1f8-94ae259bb057/j0lgaOa?width=3840&resize=contain&quality=75"
+                            className="aspect-square size-full object-cover grayscale group-hover:grayscale-0"
+                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                          />
+                        </span>
+                        <h3 className="tracking-tight line-clamp-1 text-left font-medium text-lg lg:text-lg">Alec</h3>
+                      </div>
+                      <div className="p-3 pt-0">
+                        <h4 className="font-semibold tracking-tight line-clamp-3 truncate whitespace-break-spaces text-muted-foreground text-sm pb-0 text-left">
+                          crafting the next-gen builder community & fine-tuning cod loadouts with badgunny
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Carousel for Buttons */}
+            <div className="relative max-w-sm md:max-w-md lg:max-w-2xl xl:max-w-4xl" role="region" aria-roledescription="carousel">
+              <div className="overflow-hidden">
+                <div className="border-color: rgb(30 41 59) flex -ml-4" style={{ transform: 'translate3d(-1716.7px, 0px, 0px)' }}>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max" style={{ transform: 'translate3d(2480px, 0px, 0px)' }}>
+                    <button className="cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">show me hardware people</button>
+                  </div>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max">
+                    <button className="border-color: rgb(30 41 59) cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">looking for a marketer</button>
+                  </div>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max">
+                    <button className="border-color: rgb(30 41 59) cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">need a producer for my album</button>
+                  </div>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max">
+                    <button className="border-color: rgb(30 41 59) cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">i need to hire a react engineer</button>
+                  </div>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max">
+                    <button className="border-color: rgb(30 41 59) cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">show me content creators</button>
+                  </div>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max">
+                    <button className="border-color: rgb(30 41 59) cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">experts on tiktok</button>
+                  </div>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max">
+                    <button className="border-color: rgb(30 41 59) cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">who are some people i should invest in?</button>
+                  </div>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max">
+                    <button className="border-color: rgb(30 41 59) cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">tell me the legend of naveed</button>
+                  </div>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max">
+                    <button className="border-color: rgb(30 41 59) cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">i'm building in gaming</button>
+                  </div>
+                  <div role="group" aria-roledescription="slide" className="shrink-0 grow-0 basis-full pl-4 mr-0 min-w-max max-w-max">
+                    <button className="border-color: rgb(30 41 59) cursor-pointer border px-4 py-2 text-muted-foreground hover:text-primary">show me fast growing projects</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </a>
-        <div  className="text-gray-600 flex w-full flex-col gap-0 pt-4 md:pr-6">
-          <a href="/">
-            <button  className="hover:text-gray-400 relative inline-flex items-center justify-center rounded-md font-semibold tracking-base ring-offset-background transition-colors focus-visible:outline-none disabled:opacity-50 text-base leading-none text-muted-foreground shadow-none hover:text-primary px-0 py-2 h-auto">discover</button>
-          </a>
-          <a href="/projects">
-            <button  className="hover:text-gray-400 relative inline-flex items-center justify-center rounded-md font-semibold tracking-base ring-offset-background transition-colors focus-visible:outline-none disabled:opacity-50 text-base leading-none text-muted-foreground shadow-none hover:text-primary px-0 py-2 h-auto">projects</button>
-          </a>
-          <div  className="my-3 w-full border-b border-dashed text-gray-900"></div>
-          <p  className="text-base font-medium leading-6 tracking-base pr-6 text-muted-foreground">welcome to sage, a place to find dope people building cool shit.</p>
-          <br />
-          <br />
-          <p  className="text-base font-medium leading-6 tracking-base pr-6 text-muted-foreground">this is the place where people work on ideas they are passionate about.</p>
-          <br />
-          <br />
-          <p  className="text-base font-medium leading-6 tracking-base pr-6 text-muted-foreground">give it a try.</p>
-          <br />
-          <br />
-          <p  className="text-base font-medium leading-6 tracking-base pr-6 text-muted-foreground">see you in a search soon.</p>
-        </div>
-        <div  className="mt-auto flex flex-col gap-3 pr-6">
-          <button  className="bg-white text-black relative inline-flex items-center justify-center  text-base font-semibold tracking-base ring-offset-background transition-colors focus-visible:outline-none disabled:opacity-50 bg-primary text-primary-foreground shadow-md hover:bg-primary/90 h-[42px] px-4 py-2">sign up</button>
-          <button className="relative inline-flex items-center justify-center  text-base font-semibold tracking-base shadow ring-offset-background transition-colors focus-visible:outline-none disabled:opacity-50 border border-input hover:bg-accent hover:opacity-80 h-[42px] px-4 py-2">log in</button>
         </div>
       </div>
-    </div>
-    </div>
-  )
-}
 
-export default sidebar
+      {/* Footer */}
+      <div className="sticky mt-auto md:max-w-xl bottom-0 mx-auto w-full bg-background/70 backdrop-blur-md">
+        <div className="flex flex-col gap-4 border-t py-4 md:border-none px-4 md:px-0 md:py-6">
+          <form>
+            <div className="border-color: rgb(30 41 59) relative flex w-full grow flex-col justify-center overflow-hidden border bg-background">
+              <textarea
+                tabIndex={0}
+                rows={1}
+                placeholder="message sage..."
+                spellCheck="false"
+                className="w-full resize-none overflow-hidden bg-transparent py-3 pl-4 pr-10 outline-none placeholder:text-muted-foreground focus-within:outline-none"
+                style={{ height: '48px !important' }}
+              />
+              <div className=" absolute right-2 z-10 flex items-center gap-1 sm:right-2">
+                <button
+                  disabled
+                  className="relative inline-flex items-center justify-center rounded-md text-base font-semibold tracking-base ring-offset-background transition-colors focus-visible:outline-none disabled:opacity-50 shadow-none hover:bg-accent size-8 p-0 hover:text-primary/50 cursor-pointer"
+                  type="submit"
+                  data-state="closed"
+                >
+                  <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="size-4">
+                    <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" />
+                  </svg>
+                  <span className="sr-only">what are you working on</span>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Carousel;
